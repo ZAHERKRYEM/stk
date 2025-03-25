1a0fcbdefddf4071dbad8c91b06cfb9e4bca4801
zaherkryem <145841446+ZAHERKRYEM@users.noreply.github.com>
2025-03-25 22:00:35 +0300

    13

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,31 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+
+Route::get('/server-files', function () {
+    $rootPath = base_path(); // جذر مشروع Laravel
+    return getDirectoryTree($rootPath);
+});
+
+function getDirectoryTree($dir)
+{
+    $result = [];
+    $files = array_diff(scandir($dir), ['.', '..']);
+
+    foreach ($files as $file) {
+        $filePath = "$dir/$file";
+        $result[] = is_dir($filePath) ? [
+            'name' => $file,
+            'type' => 'directory',
+            'children' => getDirectoryTree($filePath) // استدعاء تكراري للمجلدات
+        ] : [
+            'name' => $file,
+            'type' => 'file'
+        ];
+    }
+
+    return response()->json($result);
+}
