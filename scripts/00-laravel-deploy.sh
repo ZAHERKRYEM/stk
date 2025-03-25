812ce342fe71848318a4a33d4d7df8d71237f6a7
zaherkryem <145841446+ZAHERKRYEM@users.noreply.github.com>
2025-03-25 15:26:23 +0300

    fin

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,7 +3,7 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
+echo "Generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
@@ -15,4 +15,9 @@ php artisan route:cache
 echo "Running migrations..."
 php artisan migrate --force
 
+echo "Creating storage symlink..."
+php artisan storage:link
 
+
+chmod -R 775 storage bootstrap/cache
+chown -R www-data:www-data storage bootstrap/cache
